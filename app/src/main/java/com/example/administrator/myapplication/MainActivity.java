6b711fcb54c76a5de26a09ce4225761c50b88f4b
my111 <https://github.com/heixiongxiaozhao/my111.git>
2018-04-11 18:42:06 +0800

    shangchuanaaaa

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
